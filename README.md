f41b167eabbaffcc138092d661fbe7a708fdd096
Reed Thayer <digitalfeonix@gmail.com>
2019-02-02 22:24:55 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Craigslist Bot
+
+This is a simple script that will search Craigslist for you and return any result it finds via email.
+
+The `lib.email.php` and `lib.network.php` files from [DigitalFeonix/Globals-Library](https://github.com/DigitalFeonix/Globals-Library)
+are required. Place them somewhere in the PHP include path.
+
+First copy the `craigslist-bot.*` files onto a server with PHP.
+
+You will need to edit `craigslist-bot.php` to adjust the email address that the emails will be coming from in the
+following bit of code.
+
+```php
+send_multipart_email(
+    $search['ret'],
+    'webmaster@example.com',
+    'CraigslistBot Results for '.$name,
+    $text_message,
+    $html_message,
+    array_key_exists('bcc', $search) ? $search['bcc'] : ''
+);
+```
+
+Then edit the `craigslist-bot.cfg` file to match the parameters of your searches. (The one in the repo has several examples)
+
+Each section/search starts with puting a unique name in brackets. This will be used as part of the subject line for the
+email when it sends results.
+
+* `loc` is the city you are searching. When you go to Craigslist it will be part of the domain like `https://seattle.craigslist.org/`
+* `cat` is the 3 character code for the category you are searching under. To get this, navigate the website to the 
+  category you are interested in and it will be at the end of the URL. For example, the overall Jobs page for Seattle is
+  `https://seattle.craigslist.org/d/jobs/search/jjj` the code `jjj` at the end is what you are looking for.
+* `q` is the query to search for. You can do some interesting combinations, but will have to be quoted if it is not a
+  single word.
+* `ret` is the email address you would like to send the results to.
+* `bcc` is an optional email you could have it BCC'd to
+
+Finally, add the script to crontab in whatever schedule you like
+```
+0 9,12,15,18,21 * * * /home/user/craigslist-bot.php
+```
+
+Sit back and watch as the emails roll in.
---
+++ craigslist-bot.php
@@ -0,0 +1,97 @@
+#!/usr/bin/php
+<?php
+
+ini_set('default_socket_timeout', 15);
+ini_set('user_agent', 'CraigslistBot/0.1');
+include_once('lib.email.php');
+
+$last_run = unserialize(file_get_contents('craigslist-bot.dat'));
+$searches = parse_ini_file('craigslist-bot.cfg', TRUE);
+
+foreach ($searches as $name => $search)
+{
+    $md5 = md5(serialize($search));
+    $get = time();
+
+    $text_message = 'Get a real email client!';
+    $html_message = '';
+
+    $url = sprintf(
+        'https://%s.craigslist.org/search/%s?query=%s&format=rss',
+        $search['loc'],
+        $search['cat'],
+        urlencode($search['q'])
+    );
+
+    $xml = file_get_contents($url);
+
+    if ($xml === FALSE)
+    {
+        sleep(15);
+        continue;
+    }
+
+    $xml = mb_convert_encoding($xml,'UTF-8');
+
+    if (empty($xml))
+    {
+        error_log('xml was empty');
+        continue;
+    }
+
+    $rss = simplexml_load_string($xml);
+
+    if (!is_object($rss))
+    {
+        error_log('rss failed to load');
+        continue;
+    }
+
+    $syn = $rss->channel->children('syn', TRUE);
+
+    $is_first_run = !array_key_exists($md5, $last_run);
+    $this_run     = !$is_first_run ? $last_run[$md5] : 0;
+
+    foreach ($rss->item as $item)
+    {
+        $enc = $item->children('enc', TRUE);
+        $dc = $item->children('dc', TRUE);
+
+        $tstamp = strtotime($dc->date);
+
+        // put the lastest item as the tstamp to beat
+        if ($tstamp > $this_run) $this_run = $tstamp;
+
+        // skip if older than last known item
+        if (!$is_first_run && $tstamp <= $last_run[$md5]) continue;
+
+        $html_message .= '<h3 style="clear:left;margin-bottom:0px;"><a href="'.$item->link.'">'.$item->title.'</a></h3>'."\n";
+        $html_message .= '<h5 style="margin-top:0px;">Posted '.date('Y-m-d H:i:s', $tstamp).'</h5>'."\n";
+        $html_message .= '<div>'."\n";
+        if (count($enc) > 0)
+        {
+            $html_message .= '<img src="'.$enc->enclosure->attributes()->resource.'" style="float:left;margin-right:5px;margin-bottom:10px;">'."\n";
+        }
+        $html_message .= $item->description."\n";
+        $html_message .= '</div>'."\n";
+    }
+
+    $last_run[$md5] = $this_run;
+
+    if ($html_message != '')
+    {
+        send_multipart_email(
+            $search['ret'],
+            'webmaster@example.com',
+            'CraigslistBot Results for '.$name,
+            $text_message,
+            $html_message,
+            array_key_exists('bcc', $search) ? $search['bcc'] : ''
+        );
+    }
+
+    // be nice and not slam the servers
+    sleep( mt_rand(5,15) );
+}
+
+file_put_contents('craigslist-bot.dat', serialize($last_run));
